2291122fd90eebc5740155b6a6bab55552f32c65
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-27 22:24:46 -0500

    ExplorerService.test adding second test get amount by mission

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -6,6 +6,11 @@ describe('Filter and mapping operations', () => {
     const explorers = reader.readJsonFile('./data/explorers.json')
     const test = explorerService.filterByMission(explorers, 'node')
     expect(test[0].name).toBe('Woopa1')
+  });
+
+  test('2. Get amount of explorers by Mission', () => {
+    const explorers = reader.readJsonFile('./data/explorers.json')
+    const test = explorerService.filterByMission(explorers, 'node')
     expect(test.length).toBe(10)
   });
 });

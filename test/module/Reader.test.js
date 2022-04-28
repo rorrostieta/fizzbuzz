d071f205cad0f5ba585a7239806dd0210385a63d
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-27 23:12:54 -0500

    update chaning test for new test data model successful test

--- test/module/Reader.test.js
+++ test/module/Reader.test.js
@@ -3,7 +3,7 @@ const reader = require('./../../app/module/Reader');
 describe('Need to read a JSON file', () => {
   test('1. Reader.js test with test data', () => {
     const test = reader.readJsonFile('./data/test.json')
-    expect(test.name).toBe('TestName')
+    expect(test[0].name).toBe('TestName')
   });
 
   test('2. Reader.js test with explorers data', () => {

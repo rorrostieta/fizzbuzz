97b1f399a146e0ee2b4dbe56995013ee387af275
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-27 22:23:11 -0500

    ExplorerService filterByMission completed

--- app/services/ExplorerService.js
+++ app/services/ExplorerService.js
@@ -1,8 +1,9 @@
-const reader = require('./app/module/Reader');
+const reader = require('./../module/Reader');
 
 class explorerService {
   static filterByMission(explorers, mission){
     const explorersInMission = explorers.filter((explorer) => explorer.mission == mission);
+    return explorersInMission
   }
 }
 module.exports = explorerService

34def6b393c93eb24aa18de63d7bfa5cc7029894
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-28 20:07:44 -0500

    added getExplorersUsernamesByMission and tested

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,11 +1,14 @@
-// const reader = require('./../../lib/utils/Reader');
-// const explorerService = require('./../../lib/services/ExplorerService');
-// const fizzBuzzService = require('./../../lib/services/FizzBuzzService');
 const explorerController = require('./../../lib/controllers/ExplorerController');
 
 describe('Test for the ExplorerController module', () => {
   test('1. Get Explorers By Mission', () => {
     const test = explorerController.getExplorersByMission('node')
     expect(test[0].name).toBe('Woopa1')
+  });
+
+  test('2. Get Explorers Usrname By Mission', () => {
+    const test = explorerController.getExplorersUsernamesByMission('node')
+    expect(test).toContain('ajolonauta1')
+    expect(test).not.toContain('ajolonauta9')
   });
 });

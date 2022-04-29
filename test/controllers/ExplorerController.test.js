404631d61fae4a25b0bc63f655a917e7e0b4c255
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-28 20:59:09 -0500

    linter fixes

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,19 +1,19 @@
-const explorerController = require('./../../lib/controllers/ExplorerController');
+const explorerController = require("./../../lib/controllers/ExplorerController");
 
-describe('Test for the ExplorerController module', () => {
-  test('1. Get Explorers By Mission', () => {
-    const test = explorerController.getExplorersByMission('node')
-    expect(test[0].name).toBe('Woopa1')
-  });
+describe("Test for the ExplorerController module", () => {
+    test("1. Get Explorers By Mission", () => {
+        const test = explorerController.getExplorersByMission("node");
+        expect(test[0].name).toBe("Woopa1");
+    });
 
-  test('2. Get Explorers Usrname By Mission', () => {
-    const test = explorerController.getExplorersUsernamesByMission('node')
-    expect(test).toContain('ajolonauta1')
-    expect(test).not.toContain('ajolonauta9')
-  });
+    test("2. Get Explorers Usrname By Mission", () => {
+        const test = explorerController.getExplorersUsernamesByMission("node");
+        expect(test).toContain("ajolonauta1");
+        expect(test).not.toContain("ajolonauta9");
+    });
 
-  test('3. Get Explorers Amount By Mission', () => {
-    const test = explorerController.getExplorersAmountByMission('node')
-    expect(test).toBe(10)
-  });
+    test("3. Get Explorers Amount By Mission", () => {
+        const test = explorerController.getExplorersAmountByMission("node");
+        expect(test).toBe(10);
+    });
 });
